0a1429921ddd32fb683181c34416f3921b4791e4
Ilya Tomskikh <69895429+IlyaTomskikh@users.noreply.github.com>
2022-11-10 19:12:11 +0700

    Update Gitproject.java

--- src/main/java/com/mycompany/gitproject/Gitproject.java
+++ src/main/java/com/mycompany/gitproject/Gitproject.java
@@ -12,6 +12,6 @@ package com.mycompany.gitproject;
 public class Gitproject {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hello Worlddsgfkjdsgkjdrhesk!");
     }
 }
