350c267f1c4e52cf710ed59ae7209ea9f05291f0
Ilya <74zayka@gmail.com>
2022-11-10 19:15:48 +0700

    added func sum

--- src/main/java/com/mycompany/gitproject/Gitproject.java
+++ src/main/java/com/mycompany/gitproject/Gitproject.java
@@ -10,6 +10,10 @@ package com.mycompany.gitproject;
  * @author Admin
  */
 public class Gitproject {
+    
+    public static int sum(int a, int b) {
+        return a + b;
+    }
 
     public static void main(String[] args) {
         System.out.println("Hello Worlddsgfkjdsgkjdrhesk!");
