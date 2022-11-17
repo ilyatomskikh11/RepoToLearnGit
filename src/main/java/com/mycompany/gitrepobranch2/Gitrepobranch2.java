6c39ba77435a08e3884355aca4d8cb03fa190b06
IlyaTomskikh <ilya.tomskih@gmail.com>
2022-11-17 17:01:53 +0700

    added function myToString(int)

--- src/main/java/com/mycompany/gitrepobranch2/Gitrepobranch2.java
+++ src/main/java/com/mycompany/gitrepobranch2/Gitrepobranch2.java
@@ -10,6 +10,10 @@ package com.mycompany.gitrepobranch2;
  * @author Ilya
  */
 public class Gitrepobranch2 {
+    
+    public static String myToString(int a) {
+        return "It is my string from int " + a;
+    }
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
