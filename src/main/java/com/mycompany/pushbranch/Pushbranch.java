a44f778bfedb336f040b2ddd20b71f783229fd2b
Ilya <74zayka@gmail.com>
2022-11-17 18:46:54 +0700

    first commit

---
+++ src/main/java/com/mycompany/pushbranch/Pushbranch.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.pushbranch;
+
+/**
+ *
+ * @author Admin
+ */
+public class Pushbranch {
+    
+    public static String myToStrinh(int asdf) {
+        return "String" + asdf;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
